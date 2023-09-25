7b3e65c49c59033593141a0cc2aa58ba10f286b6
MtahaK <taha.khokhar@mail.utoronto.ca>
2023-09-24 23:50:39 -0400

    Added email form and checking to index.html page

--- hello.py
+++ hello.py
@@ -3,8 +3,8 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, ValidationError, EmailField
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess and completely random string'
@@ -13,18 +13,33 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = EmailField('What is your UofT Email address?', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
+
         if old_name is not None and old_name != form.name.data:
-            flash('Seems like you have changed your name!')
+            flash('Looks like you have changed your name!')
+        
+        
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+
+        if 'utoronto' not in form.email.data.lower():
+            session['email_warning'] = 'Please use a UofT Email address.'
+        else:
+            session.pop('email_warning', None)
+        
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form = form, name = session.get('name'))
+    return render_template('index.html', form = form, name = session.get('name'), email=session.get('email'), email_warning=session.get('email_warning'))
 
 @app.route('/user/<name>')
 def user(name):
